8e593443f565c3a94229b47d7a99929e9dc8ed4d
dogametal <dogametal@hotmail.com>
2021-02-12 07:24:06 -0300

    Get Posts and fill into template

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,23 @@
-const xrh = new XMLHttpRequest();
+const xhr = new XMLHttpRequest();
+const listElement = document.querySelector('.posts');
+const postTemplate = document.getElementById('single-post');
 
-xrh.open('GET', 'http://jsonplaceholder.typicode.com/posts');
-xrh.send();
+xhr.open('GET', 'http://jsonplaceholder.typicode.com/posts');
+xhr.responseType = 'json';
+
+xhr.onload = function(){
+    //const listOfPosts = JSON.parse(xhr.response);
+    const listOfPosts = xhr.response;
+    console.log(listOfPosts);
+    
+    for(const post of listOfPosts){
+        const postEl = document.importNode(postTemplate.content,true);
+        postEl.querySelector('h2').textContent = post.title.toUpperCase();
+        postEl.querySelector('p').textContent = post.body;
+        listElement.append(postEl);
+    }
+    
+};
+
+xhr.send();
+
